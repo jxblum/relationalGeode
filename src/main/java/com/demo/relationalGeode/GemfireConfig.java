94caa768afbb55a11a5f034df04919cc5c682abe
John Blum <jblum@vmware.com>
2021-12-08 15:53:10 -0800

    Add configurations for the client/server topology and a (local) peer cache topology.
    Set member name.
    
    Apply general polishing.

--- src/main/java/com/demo/relationalGeode/GemfireConfig.java
+++ src/main/java/com/demo/relationalGeode/GemfireConfig.java
@@ -1,27 +1,40 @@
 package com.demo.relationalGeode;
 
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.gemfire.config.annotation.ClientCacheConfigurer;
 import org.springframework.data.gemfire.config.annotation.EnableCachingDefinedRegions;
 import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
+import org.springframework.data.gemfire.config.annotation.PeerCacheApplication;
 import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
 import org.springframework.data.gemfire.support.ConnectionEndpoint;
 import org.springframework.geode.config.annotation.EnableClusterAware;
+import org.springframework.geode.config.annotation.UseMemberName;
 
-import java.util.Arrays;
-import java.util.Collections;
-
-@EnableClusterAware
+@Configuration
+@EnableCachingDefinedRegions
 @EnableEntityDefinedRegions(basePackageClasses = {SomeObject.class})
 @EnableGemfireRepositories(basePackageClasses = {SomeObjectRepository.class})
-@EnableCachingDefinedRegions
-@Configuration
+@UseMemberName("RelationalGeode")
+@SuppressWarnings("unused")
 public class GemfireConfig {
+
+    @Configuration
+    @EnableClusterAware
+    @Profile("client-cache")
+    static class ClientCacheGemfireConfig { }
+
+    @Configuration
+    @PeerCacheApplication
+    @Profile("peer-cache")
+    static class PeerCacheGemfireConfig { }
 
     @Bean
     @ConditionalOnProperty(prefix = "geode", name = "host")
@@ -29,17 +42,14 @@ public class GemfireConfig {
         return ((beanName, bean) -> bean.setServers(Collections.singletonList(new ConnectionEndpoint(host, port))));
     }
 
-
     @Bean
     ApplicationListener<ApplicationReadyEvent> initializeCache(SomeObjectRepository repository) {
-        return event -> {
-            repository.save(SomeObject.builder()
-                    .someId("id1")
-                    .customFirstObjects(Arrays.asList(CustomFirstObject.builder()
-                            .id(11)
-                            .amount(2.0)
-                            .build()))
-                    .build());
-        };
+        return event -> repository.save(SomeObject.builder()
+            .someId("id1")
+            .customFirstObjects(Collections.singletonList(CustomFirstObject.builder()
+                .id(11)
+                .amount(2.0)
+                .build()))
+            .build());
     }
 }
