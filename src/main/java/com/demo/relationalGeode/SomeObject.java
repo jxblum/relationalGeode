79538c72e440ac8e0e4a142767ac58cd0fa4b333
John Blum <jblum@vmware.com>
2021-12-08 16:28:58 -0800

    Opimize imports.

--- src/main/java/com/demo/relationalGeode/SomeObject.java
+++ src/main/java/com/demo/relationalGeode/SomeObject.java
@@ -1,14 +1,14 @@
 package com.demo.relationalGeode;
 
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.gemfire.mapping.annotation.Region;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.gemfire.mapping.annotation.Region;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @Data
 @Builder
