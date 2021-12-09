92c5c69c16f77651997d8f1c35daf99b17296b6c
John Blum <jblum@vmware.com>
2021-12-08 18:20:48 -0800

    Refactor the CustomFirstObject class and implement java.io.Serializable.

--- src/main/java/com/demo/relationalGeode/CustomFirstObject.java
+++ src/main/java/com/demo/relationalGeode/CustomFirstObject.java
@@ -1,11 +1,13 @@
 package com.demo.relationalGeode;
 
+import java.io.Serializable;
+
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class CustomFirstObject {
+public class CustomFirstObject implements Serializable {
     private Integer id;
     private Double amount;
 }
